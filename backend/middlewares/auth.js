c6f94dc53f7cec45b36a27bafd6779a2995c184c
unbugster <unbugster@gmail.com>
2023-05-15 21:06:15 +0300

    Fix after review

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -18,7 +18,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
   } catch (err) {
-    next(new customError.UnauthorizedError('Необходима авторизация!'));
+    return next(new customError.UnauthorizedError('Необходима авторизация!'));
   }
 
   req.user = payload;
